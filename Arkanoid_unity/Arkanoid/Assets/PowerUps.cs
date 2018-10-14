be58360b493360b6c2ebc640932703120a845f17
Pahlorus <katyshev1980@yandex.ru>
2018-10-14 22:47:12 +0200

    Made minor fixes
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Arkanoid_unity/Arkanoid/Assets/PowerUps.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Arkanoid
+{
+    internal class PowerUps : MonoBehaviour
+    {
+        [SerializeField]
+        protected GameObject _pellet;
+        [SerializeField]
+        protected GameObject _bat;
+        [SerializeField]
+        protected Sprite _sprite;
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+    }
+
+
+}
+
