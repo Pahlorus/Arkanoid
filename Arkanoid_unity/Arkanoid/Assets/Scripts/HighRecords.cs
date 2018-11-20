c8b64b4204e412922a78eceef5c2a37ddb90cf81
Pahlorus <katyshev1980@yandex.ru>
2018-11-20 15:18:12 +0200

    Add class HighRecord Add Dropdown for input Player Name
    Signed-off-by: Pahlorus <katyshev1980@yandex.ru>

---
+++ Arkanoid_unity/Arkanoid/Assets/Scripts/HighRecords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkanoid
+{
+    class HighRecords
+    {
+        private string _playerName;
+        private int _score;
+
+    }
+}
